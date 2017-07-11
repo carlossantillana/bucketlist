03f2255509c0d0888accd02067a2900081367ad6
carlos <csant019@ucr.edu>
2017-07-11 00:23:52 -0700

    Submits Item now works, next goal to implement restrictions on input, and protect from sql injections

--- submit.php
+++ submit.php
@@ -1,14 +1,17 @@
 <?php
-//Initializing variables
-$host = "localhost";
-$user = "root";
-$password = "Lapras#131";
-$database = "bucketList";
-$getItem = array();
-$getItem2 = array();
-$newItem = $_POST['enterItem'];
-//start connection
-$conn = new mysqli($host, $user, $password, $database);
-$sql = "INSERT INTO items(item) VALUE ('$newItem')";
-$conn->query($sql);//Insert into database
+   if(isset($_POST['enterItem'])) {
+        //Initializing variables
+        $host = "localhost";
+        $user = "root";
+        $password = "Lapras#131";
+        $database = "bucketList";
+        $getItem = array();
+        $newItem = $_POST['enterItem'];
+        //start connection
+        $conn = mysqli_connect($host, $user, $password, $database) or die("Cound not connect! <br>" . mysqli_connect_error() );
+        $sql = "INSERT INTO items(item) VALUE ('$newItem')";
+        $conn->query($sql);//Insert into database
+        mysqli_close($conn);
+}
+header('Location: index.html');
 ?>
